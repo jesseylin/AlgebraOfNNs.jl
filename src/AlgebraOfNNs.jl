b0d41275ac484eab4ce578eb26f1456a28776dd8
Jesse Young Lin <jesseyounglin@gmail.com>
2024-01-13 02:20:30 +0100

    chore: add LuxCore to deps

--- src/AlgebraOfNNs.jl
+++ src/AlgebraOfNNs.jl
@@ -4,6 +4,7 @@ export @lift_nn
 
 using Lux: Parallel, Chain, WrappedFunction
 using MacroTools: postwalk
+using LuxCore: AbstractExplicitLayer
 
 include("./operator_morphisms.jl")
 include("./lift.jl")

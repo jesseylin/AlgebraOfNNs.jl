c2e98dd827c44a724f932ddb494581e929be8389
Jesse Young Lin <jesseyounglin@gmail.com>
2024-01-13 02:22:03 +0100

    refactor: rename some functions and move them

--- src/operator_morphisms.jl
+++ src/operator_morphisms.jl
@@ -1,21 +1,23 @@
-function lift_number(y::Number)
+function lift(y::Number)
     return WrappedFunction(x -> fill(y, size(x)))
 end
+
+function lift_unary(func, op)
+    return quote
+        Lux.Chain($op, Lux.WrappedFunction(func))
+    end
+end
+function lift_nary(func, ops...)
+    ops = [op isa Number ? lift(op) : op for op in ops]
+    return quote
+        Lux.Parallel($func, $(ops...))
+    end
+end
+
+
 function lift_mult(scalar::Number, op)
     return quote
         Lux.Chain($op, Lux.WrappedFunction(x -> $scalar * x))
     end
 end
 lift_mult(op, scalar::Number) = lift_mult(scalar, op)
-
-function lift_nary(func, ops...)
-    ops = [op isa Number ? lift_number(op) : op for op in ops]
-    return quote
-        Lux.Parallel($func, $(ops...))
-    end
-end
-function lift_unary(func, op)
-    return quote
-        Lux.Chain($op, Lux.WrappedFunction(func))
-    end
-end

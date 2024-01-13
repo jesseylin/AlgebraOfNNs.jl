fb028208b6de21d69c8326317752ccdf29af91ea
Jesse Young Lin <jesseyounglin@gmail.com>
2024-01-13 02:36:48 +0100

    style: format

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ rng = Random.Xoshiro(1234)
 @testset "Integration tests" begin
     import Lux
     using Lux: Dense
-    l1 = Dense(1=>2)
-    l2 = Dense(1=>3)
+    l1 = Dense(1 => 2)
+    l2 = Dense(1 => 3)
 
     init(x) = Lux.setup(rng, x)
     # binary addition
@@ -20,11 +20,11 @@ rng = Random.Xoshiro(1234)
     @test init(@lift_nn 1 + 2) isa Any
 
     # basic scalar mult
-    @test init(@lift_nn 2*l1) isa Any
+    @test init(@lift_nn 2 * l1) isa Any
 
     # complicated scalar mult
     # but please don't write formulas like this
-    @test init(@lift_nn 2*2*l1) isa Any
+    @test init(@lift_nn 2 * 2 * l1) isa Any
 
     # basic unary
     @test init(@lift_nn -l1) isa Any
